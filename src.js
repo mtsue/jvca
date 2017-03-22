0058e6a092cc609fdab9d905a0b8b9b9b7310377
mtsue <bolerock309@gmail.com>
2017-03-22 11:04:32 +0900

    csvファイル書き込み機能追加

--- src.js
+++ src.js
@@ -3,6 +3,7 @@
 /* modules */
 const client = require('cheerio-httpcli');
 const async = require('async');
+const fs = require('fs');
 
 /* URL */
 const mainURL = 'http://jvndb.jvn.jp';
@@ -58,41 +59,62 @@ for(let i = 0; i < access.length; i++){
 }
 
 var accessDetail = [];
+/* getURL async処理 */
 async.series(getURL,function(callback){
+
     for(let i = 0; i < detailURL.length; i++){
         accessDetail[i] = client.fetch(detailURL[i]);
     }
     var getDetail = [];
+
+    var ids = [];
+    var products = [];
+    var problems = [];
+    var dates = [];
+
     for(let i = 0; i < accessDetail.length; i++){
         getDetail[i] = function(callback){
             accessDetail[i].then(function(data){
                 var $ = data.$;
 
-                var id = '';
-                var product = '';
-                var probrem = '';
-                var date = '';
-
                 $('.vuln_table_clase').each(function(){
                     if($(this).children('tr').eq(0).text().trim() == '[English]'){
-                        id = $(this).children('tr').eq(1).text().trim();
-                        product = $(this).children('tr').eq(10).text().trim();
-                        probrem = $(this).children('tr').eq(4).text().trim();
+                        ids.push($(this).children('tr').eq(1).text().trim());
+                        products.push($(this).children('tr').eq(10).text().trim());
+                        problems.push($(this).children('tr').eq(4).text().trim());
                     }else{
-                        id = $(this).children('tr').eq(0).text().trim();
-                        product = $(this).children('tr').eq(9).text().trim();
-                        probrem = $(this).children('tr').eq(3).text().trim();
+                        ids.push($(this).children('tr').eq(0).text().trim());
+                        products.push($(this).children('tr').eq(9).text().trim());
+                        problems.push($(this).children('tr').eq(3).text().trim());
                     }
                 });
-                date = $('.vuln_table_clase_footer').children('tr').eq(3).children('td').eq(1).text().trim();
+                dates.push($('.vuln_table_clase_footer').children('tr').eq(3).children('td').eq(1).text().trim());
 
-                console.log(id);
                 callback();
             });
         }
     }
 
-    async.series(getDetail);
+    /* getDetail async処理 */
+    async.series(getDetail, function(callback){
+
+        var filename = 'output' + Math.random() + '.csv';
+        var writeFile = [];
+        writeFile[0] = function(callback){
+            fs.writeFile(filename, '', function(hoge){
+                console.log('create' + filename);
+                callback();
+            });
+        }
+        for(let i = 1; i < ids.length; i++){
+            writeFile[i] = function(callback){
+                fs.appendFile(filename, '"' + ids[i] + '"' + ','  + ',' + '"' + products[i] + '"' + ',' + '"' + problems[i] + '"' + ',' + '起票無' + ',' + '"' + '製品と直接的な関係が無いと思われる' + '"' + ',' + '"' + detailURL[i] +'"' + ',' + '"' + dates[i] + '"' + '\n', function(hoge){
+                    callback();
+                });
+            }
+        }
+        async.series(writeFile);
+    });
 });
 
 /*
